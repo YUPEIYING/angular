01fff7bc868ce6d67954fb4465b69e7a36176b9e
DESKTOP-QA99A53\MIS-204 <sunnysmile159@gmail.com>
2018-07-10 17:26:23 +0800

    no message

--- angular-app/src/app/heroes/heroes.component.ts
+++ angular-app/src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
@@ -8,19 +9,23 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+
+  selectedHero: Hero;
+
   heroes: Hero[];
 
   constructor(private heroService: HeroService) { }
 
-  getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+  ngOnInit() {
+    this.getHeroes();
   }
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
-  ngOnInit() {
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 }
