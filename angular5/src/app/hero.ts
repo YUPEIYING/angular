dc3a7a657ea646e2502456a9a6d5b14087815d65
DESKTOP-QA99A53\MIS-204 <sunnysmile159@gmail.com>
2018-07-11 08:30:03 +0800

    no message

--- angular5/src/app/hero.ts
+++ angular5/src/app/hero.ts
@@ -1,15 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
-})
-export class HeroesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class Hero {
+  id: number;
+  name: string;
 }
